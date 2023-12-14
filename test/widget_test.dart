dc7635ef72e128477f492ca2a730a9e9da169259
Wagner de Araujo <124446545+WagDevX@users.noreply.github.com>
2023-12-14 18:13:52 -0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,22 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:healthcare/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('Vamos calcular o seu IMC?'), findsOneWidget);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.calculate));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
